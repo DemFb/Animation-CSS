2e073c31ea549e470032837f05ae9ebc507c0747
Fabala Dembele <dembele.fabala@gmail.com>
2020-05-26 22:36:17 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,29 @@
+const text = document.querySelector(".fancy");
+const strText = text.textContent;
+const splitText = strText.split("");
+text.textContent = "";
+
+for(var i = 0; i < splitText.length; i++) {
+    text.innerHTML += "<span>" + splitText[i] + "</span>";
+}
+
+
+let char = 0;
+let timer = setInterval(onTick, 50);
+
+function onTick() {
+    const span = text.querySelectorAll('span')[char];
+    span.classList.add('fade');
+    char++
+
+    if(char === splitText.length){
+        complete();
+        return;
+    }
+}
+
+function complete() {
+
+    clearInterval(timer);
+    timer = null;
+}
